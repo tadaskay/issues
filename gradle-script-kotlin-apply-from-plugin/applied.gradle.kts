eca02cbe78c50c8c1af8ca46bcb06fa067896696
Tadas Krivickas <tadas.krivickas@gmail.com>
2016-11-18 00:54:15 +0200

    Move plugin config to the applied script

--- gradle-script-kotlin-apply-from-plugin/applied.gradle.kts
+++ gradle-script-kotlin-apply-from-plugin/applied.gradle.kts
@@ -1,3 +1,13 @@
 import org.gradle.plugin.HelloWorldPlugin
 
+buildscript {
+    repositories {
+        jcenter()
+        gradleScriptKotlin()
+    }
+    dependencies {
+        classpath("org.gradle:gradle-hello-world-plugin:0.2")
+    }
+}
+
 apply<HelloWorldPlugin>()
--- gradle-script-kotlin-apply-from-plugin/build.gradle.kts
+++ gradle-script-kotlin-apply-from-plugin/build.gradle.kts
@@ -1,11 +1,1 @@
-buildscript {
-    repositories {
-        jcenter()
-        gradleScriptKotlin()
-    }
-    dependencies {
-        classpath("org.gradle:gradle-hello-world-plugin:0.2")
-    }
-}
-
 applyFrom("applied.gradle.kts")
